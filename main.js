7ec1878a69b0840f75cdb6b61856a477a7abe8e5
Kjell <kjell.gudrun-schoke@hotmail.com>
2020-04-14 12:15:09 +0200

    test commit 3

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const { app, BrowserWindow } = require('electron');
 function createWindow () {
 	const win = new BrowserWindow({
 		width: 1000,
-		height: 600,
+		height: 1000,
 		webPreferences: {
 		nodeIntegration: true
 		}
