ac558798f2356ac15fb1ddb92b4bab1ca30a2ce2
Valdemar Lorenzen <creme3sandwich@gmail.com>
2020-04-16 10:08:56 +0200

    organized and added navbar

---
+++ js/afleveringer.js
@@ -0,0 +1,3 @@
+const { Builder, By, Key, Until } = require('selenium-webdriver');
+
+
--- main.js
+++ main.js
@@ -9,7 +9,7 @@ function createWindow () {
 		}
 	});
 
-	win.loadFile('index.html');
+	win.loadFile('html/afleveringer.html');
 
 	//win.webContents.openDevTools();
 }
