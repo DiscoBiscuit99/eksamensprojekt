aa4fb8fd747435df77ecce7fdd9a2e5c45545571
Kjell Schoke <kjell.gudrun-schoke@hotmail.com>
2020-05-06 21:12:06 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ async function example() {
   let driver = await new Builder().withCapabilities({'browserName': 'firefox', acceptSslCerts: true, acceptInsecureCerts: true}).build()
   await driver.get("https://ludus.rybners.dk/samllogin")
 
-  await driver.findElement(By.id("userNameInput")).sendKeys("kjel0393");
-  await driver.findElement(By.id("passwordInput")).sendKeys("Kjell250968");
+  await driver.findElement(By.id("userNameInput")).sendKeys("USERNAME");
+  await driver.findElement(By.id("passwordInput")).sendKeys("PASSWORD");
   await driver.findElement(By.id("submitButton")).sendKeys(Key.RETURN);
 }
 example();
