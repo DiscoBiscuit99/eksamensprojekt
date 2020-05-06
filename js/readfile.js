f759b4d83eb60baaed31f4bc219d07a245a7e7dd
Kjell <kjell.gudrun-schoke@hotmail.com>
2020-05-06 21:07:43 +0200

    added selenium to log in into ludus

--- js/readfile.js
+++ js/readfile.js
@@ -1,13 +1,17 @@
 const fs = require('fs')
 const dirfile = "../files/";
 
+function moveToMat(oldPath, newPath) {
+  fs.rename(oldPath, newPath);
+}
+
 fs.readdir(dirfile, (err, files) => {
   files.forEach(file => {
     if((file.toLowerCase().includes("programmering")) || (file.toLowerCase().includes("prog"))) {
-      console.log(file);
+
     }
     else if ((file.toLowerCase().includes("matematik")) || (file.toLowerCase().includes("mat"))) {
-      console.log(file);
+
     }
     else if (file.toLowerCase().includes("dansk")) {
       console.log(file);
