6bf536fbb7473e18e11ee8a471641e865d1fb529
Valdemar Lorenzen <valdemar.lorenzen@gmail.com>
2020-05-08 22:29:29 +0200

    deligated scraping to the file  and cleaned it a bit

--- js/_start.js
+++ js/_start.js
@@ -1,3 +1,4 @@
+const {Builder, By, Key, until, promise} = require('selenium-webdriver');
 const fs = require('fs');
 
 let userdataJSON = fs.readFileSync('./user-credentials.json');
@@ -8,10 +9,6 @@ let username = userdata.user[0].name;
 let password = userdata.user[0].password;
 
 console.log("Username:", username + ", Password:", password);
-
-const {Builder, By, Key, until, promise} = require('selenium-webdriver');
-
-let afleveringstabel;
 
 (async function scrapeLudus() {
     let driver = await new Builder().withCapabilities({
@@ -32,6 +29,9 @@ let afleveringstabel;
         await driver.wait(until.elementLocated(By.className('v-window-closebox'))).click();
 
         let menuKnapper = await driver.findElements(By.className('v-menubar-menuitem-caption'));
+
+        /// Scrape under lektietabbet. ///
+
         let lektieKnap = menuKnapper[3];
 
         await lektieKnap.click();
@@ -39,16 +39,34 @@ let afleveringstabel;
         await driver.wait(until.titleIs('Lektier'));
         await driver.sleep(1000);
 
-        let lektieMenuKnapper = await driver.findElements(By.className('v-caption'));
-
         console.log("sleep");
         await driver.sleep(1000);
         console.log("wake");
 
         let lektietabel = await driver.findElements(By.className('v-table-cell-wrapper'));
+
+        // Clear the file's contents.
+        fs.writeFile('lektier.txt', '', 'utf8', (err) => {
+            if (err) throw err;
+        });
+
+        // Initialize iterator.
+        let i = 0;
+        promise.filter(lektietabel, (element) => {
+            element.getText().then((tekst) => {
+                console.log(tekst);
+
+                // Append the new text.
+                fs.appendFile('lektier.txt', tekst + '\n', 'utf8', (err) => {
+                    if (err) throw err;
+                    console.log('Text appended to lektier.txt');
+                });
+            });
+        });
     
         /// Scrape under afleveringstabbet. ///
 
+        let lektieMenuKnapper = await driver.findElements(By.className('v-caption'));
         let afleveringsKnap = await lektieMenuKnapper[1];
 
         await afleveringsKnap.click();
@@ -59,15 +77,13 @@ let afleveringstabel;
 
         let afleveringstabel = await driver.findElements(By.className('v-table-cell-wrapper'));
 
-        let fs = require('fs');
-
-        // Clear the files contents.
+        // Clear the file's contents.
         fs.writeFile('afleveringer.txt', '', 'utf8', (err) => {
             if (err) throw err;
         });
 
-        // Initialize iterator.
-        let i = 0;
+        // Reset iterator.
+        i = 0;
         promise.filter(afleveringstabel, (element) => {
             element.getText().then((tekst) => {
                 console.log(tekst)
